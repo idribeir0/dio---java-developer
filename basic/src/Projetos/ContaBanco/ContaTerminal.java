289b360637675bc7b87687bbb3c9cabb6fb79c2e
Ione Ribeiro <idribeiro08@gmail.com>
2024-03-09 02:11:50 -0300

    Projeto criação de conta de usuario

--- basic/src/Projetos/ContaBanco/ContaTerminal.java
+++ basic/src/Projetos/ContaBanco/ContaTerminal.java
@@ -9,20 +9,25 @@ public class ContaTerminal {
         System.out.print("Por favor, digite o número da conta: ");
         int numero = scanner.nextInt();
 
+        //consumindo a quebra de linha
         scanner.nextLine();
 
+        //pedindo ao usuario que insira o numero da agencia
         System.out.print("Por favor, insira o numero da agencia: ");
         String agencia = scanner.nextLine();
 
+        ///pedindo ao usuario que insira o nome
         System.out.print("Por favor, insira o seu nome: ");
         String nome = scanner.nextLine();
 
+        //pedindo ao usuario que insira o saldo
         System.out.print("Por favor, insira o seu saldo: ");
         Double saldo = scanner.nextDouble();
 
-    
+        //print da mensagem informando o usuario que a conta foi criada
         System.out.print("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponivel para saque");
     
+        //fechando o scanner
         scanner.close();
     }
 }
